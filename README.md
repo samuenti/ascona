d37720c1cefd8cebf9fe289b4545ffb504fd91c5
jakebywater <development@jakebywater.com>
2026-01-04 16:55:38 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ascona
+
+Use SVG icons in Ruby.
+
+## Installation
+
+```ruby
+gem 'ascona'
+```
+
+Then `bundle install`.
+
+## License
+
+MIT
---
+++ lib/ascona.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ascona/version"
+
+module Ascona
+  class Error < StandardError; end
+  # Your code goes here...
+end
